3e5c807e98443eb220f135185e6ada535d44b71c
Jens Meydam <jmeydam@gmail.com>
2020-08-04 16:15:13 +0200

    Added roxygen comments.

--- R/models.R
+++ R/models.R
@@ -1,72 +1,72 @@
 #' No-pooling model
 #'
-#' For the no-pooling model we estimate theta_i using only the observed
-#' counts of observational unit i. For each unit i our no-pooling estimate
-#' of theta_i is the ratio of the observed count values of interest y_i to
-#' the reference count values n_i.
+#' For the no-pooling model we estimate \emph{theta_i} using only the observed
+#' counts of observational unit \emph{i}. For each unit \emph{i} our no-pooling
+#' estimate of \emph{theta_i} is the ratio of the observed count values of
+#' interest \emph{y_i} to the reference count values \emph{n_i}.
 #'
 #' @param n Previous reference count values (measure of exposure)
 #' @param y Previous count values of interest
-#' @return No-pooling estimates of theta
+#' @return No-pooling estimates of \emph{theta}
 theta_nopool <- function(n, y) {
   y / n
 }
 
 #' Complete-pooling model
 #'
-#' For the complete-pooling model we consider theta_i to be the same
+#' For the complete-pooling model we consider \emph{theta_i} to be the same
 #' for all observational units. We assume that observed data can be
-#' treated as coming from a homogeneous source. For all units i our
-#' complete-pooling estimate of theta_i is the ratio of the sum of
-#' the observed count values of interest y_i to the sum of the
-#' reference count values n_i.
+#' treated as coming from a homogeneous source. For all units \emph{i} our
+#' complete-pooling estimate of \emph{theta_i} is the ratio of the sum of
+#' the observed count values of interest \emph{y_i} to the sum of the
+#' reference count values \emph{n_i}.
 #'
 #' @param n Previous reference count values (measure of exposure)
 #' @param y Previous count values of interest
-#' @return Complete-pooling estimates of theta
+#' @return Complete-pooling estimates of \emph{theta}
 theta_complpool <- function(n, y) {
   rep(sum(y) / sum(n), length(n))
 }
 
 #' Partial-pooling model (Bayesian hierarchical model)
 #'
-#' The partial-pooling model can be seen as a compromise between the
-#' extremes of the no-pooling and partial-pooling models. We do not treat
-#' all observed data as coming from a homogeneous source, but we do assume
-#' that information on other units can be useful for estimating theta_i
-#' of a particular unit i, especially when there is little information
-#' for a particular unit i.
+#' The partial-pooling model can be seen as a compromise between the extremes
+#' of the no-pooling and partial-pooling models. We do not treat all observed
+#' data as coming from a homogeneous source, but we do assume that information
+#' on other units can be useful for estimating \emph{theta_i} of a particular
+#' unit \emph{i}, especially when there is little information for a particular
+#' unit \emph{i}.
 #'
 #' The partial-pooling model uses a hierarchical structure of probability
 #' distributions. We will use the same types of distributions for this model
 #' that we used for simulating the data.
 #'
-#' In particular, we assume that each observational unit i has a Poisson
-#' distribution with parameter lambda_i. lambda_i is a product
-#' of a rate parameter theta_i and the exposure n_i.
+#' In particular, we assume that each observational unit \emph{i} has a Poisson
+#' distribution with parameter \emph{lambda_i}. \emph{lambda_i} is a product of
+#' a rate parameter \emph{theta_i} and the exposure \emph{n_i}.
 #'
-#' We use a half-normal distribution with a parameter alpha for the rate
-#' parameters theta_i. This time, alpha itself is also assumed to have
-#' a probability distribution. We choose a half-normal distribution with a
-#' fixed parameter 0.376, so that the expected value of alpha is 0.3.
+#' We use a half-normal distribution with a parameter \emph{alpha} for the rate
+#' parameters \emph{theta_i}. This time, \emph{alpha} itself is also assumed to
+#' have a probability distribution. We choose a half-normal distribution with a
+#' fixed parameter 0.376, so that the expected value of \emph{alpha} is 0.3.
 #'
-#' Note that when simulating the data we set alpha to 0.05.
-#' The "true" value of alpha is substantially lower than the value
-#' initially assumed by our model.
+#' Note that when simulating the data we set \emph{alpha} to 0.05. The "true"
+#' value of \emph{alpha} is substantially lower than the value initially
+#' assumed by our model.
 #'
-#' Given data in the form of reference counts n_i (exposure) and
-#' count values of interest y_i, our model will allow us to determine
-#' a posterior probability distribution both for the parameter alpha
-#' and for the rate parameters theta_i.
+#' Given data in the form of reference counts \emph{n_i} (exposure) and count
+#' values of interest \emph{y_i}, our model will allow us to determine a
+#' posterior probability distribution both for the parameter \emph{alpha} and
+#' for the rate parameters \emph{theta_i}.
 #'
-#' We will use the mean of the theta samples drawn from the posterior
-#' distribution as the partial pooling estimate for theta.
+#' We will use the mean of the \emph{theta} samples drawn from the posterior
+#' distribution as the partial pooling estimate for \emph{theta}.
 #'
 #' @param unit Index for units
 #' @param n Previous reference count values (measure of exposure)
 #' @param y Previous count values of interest
 #' @param random_seed Seed value for Stan
-#' @return Partial-pooling estimates of theta
+#' @return Partial-pooling estimates of \emph{theta}
 theta_partpool <- function(unit, n, y, random_seed) {
   # The function ulam() returns samples drawn from the posterior
   # distribution of our model.
@@ -90,30 +90,36 @@ theta_partpool <- function(unit, n, y, random_seed) {
   post_theta_means
 }
 
-#' Add no-pooling estimate of theta to data frame
+#' Add no-pooling estimate of \emph{theta} to data frame
+#'
+#' Add no-pooling estimate of \emph{theta} to data frame.
 #'
 #' @param d Initialized data frame
-#' @return data frame with values for theta_nopool
+#' @return data frame with values for \emph{theta_nopool}
 add_theta_nopool <- function(d) {
   d$theta_nopool <- theta_nopool(d$n, d$y)
   d
 }
 
-#' Add complete-pooling estimate of theta to data frame
+#' Add complete-pooling estimate of \emph{theta} to data frame
+#'
+#' Add complete-pooling estimate of \emph{theta} to data frame.
 #'
 #' @param d Initialized data frame
-#' @return data frame with values for theta_complpool
+#' @return data frame with values for \emph{theta_complpool}
 add_theta_complpool <- function(d) {
   d$theta_complpool <- theta_complpool(d$n, d$y)
   d
 }
 
-#' Add partial-pooling estimate of theta (based on Bayesian hierarchical
-#' model) to data frame
+#' Add partial-pooling estimate of \emph{theta} to data frame
+#'
+#' Add partial-pooling estimate of \emph{theta} (based on Bayesian hierarchical
+#' model) to data frame.
 #'
 #' @param d Initialized data frame
 #' @param random_seed Seed value for Stan (default: 200731)
-#' @return data frame with values for theta_partpool
+#' @return data frame with values for \emph{theta_partpool}
 add_theta_partpool <- function(d, random_seed = 200731) {
   d$theta_partpool <- theta_partpool(d$unit, d$n, d$y, random_seed)
   d
--- R/ucls.R
+++ R/ucls.R
@@ -1,16 +1,17 @@
 #' Calculate upper control limit (UCL)
 #'
-#' Basic formula for UCL: estimated mean + 3 * estimated standard deviation
+#' Basic formula for UCL:
+#' \eqn{estimated mean + 3 * estimated standard deviation}
 #'
 #' In this case, assuming a Poisson distribution, both the estimated mean
-#' and the estimated variance are lambda_hat = theta_hat * n_new.
+#' and the estimated variance are \deqn{lambda_hat = theta_hat * n_new}.
 #'
 #' Round the result to the nearest integer.
 #'
 #' Add 0.5, so that count values are either above or below the UCL
 #' (as in Wheeler & Chambers [1992]).
 #'
-#' @param theta_hat Estimate (unless known) of rate parameter theta
+#' @param theta_hat Estimate (unless known) of rate parameter \emph{theta}
 #' @param n_new New value for exposure (reference counts)
 #' @return UCLs
 ucl <- function(theta_hat, n_new) {
@@ -21,10 +22,11 @@ ucl <- function(theta_hat, n_new) {
 
 #' Add "true" UCL to data frame
 #'
-#' UCL based on n_new and true value of theta
+#' Add UCL based on \emph{n_new} and true value of \emph{theta} to
+#' data frame.
 #'
 #' @param d Initialized data frame
-#' @return data frame with values for ucl_true_theta
+#' @return data frame with values for \emph{ucl_true_theta}
 add_ucl_true_theta <- function(d) {
   d$ucl_true_theta <- ucl(d$true_theta, d$n_new)
   d
@@ -32,10 +34,11 @@ add_ucl_true_theta <- function(d) {
 
 #' Add no-pooling UCL to data frame
 #'
-#' UCL based on n_new and no-pooling estimate of theta
+#' Add UCL based on \emph{n_new} and no-pooling estimate of \emph{theta}
+#' to data frame.
 #'
 #' @param d Initialized data frame
-#' @return data frame with values for ucl_nopool
+#' @return data frame with values for \emph{ucl_nopool}
 add_ucl_nopool <- function(d) {
   d$ucl_nopool <- ucl(d$theta_nopool, d$n_new)
   d
@@ -43,21 +46,23 @@ add_ucl_nopool <- function(d) {
 
 #' Add complete-pooling UCL to data frame
 #'
-#' UCL based on n_new and complete-pooling estimate of theta
+#' Add UCL based on \emph{n_new} and complete-pooling estimate of \emph{theta}
+#' to data frame.
 #'
 #' @param d Initialized data frame
-#' @return data frame with values for ucl_complpool
+#' @return data frame with values for \emph{ucl_complpool}
 add_ucl_complpool <- function(d) {
   d$ucl_complpool <- ucl(d$theta_complpool, d$n_new)
   d
 }
 
-#' Add partial-pooling UCL (based on Bayesian hierarchical model) to data frame
+#' Add partial-pooling UCL to data frame
 #'
-#' UCL based on n_new and partial-pooling estimate of theta
+#' Add UCL based on \emph{n_new} and partial-pooling estimate of \emph{theta}
+#' to data frame.
 #'
 #' @param d Initialized data frame
-#' @return data frame with values for ucl_partpool
+#' @return data frame with values for \emph{ucl_partpool}
 add_ucl_partpool <- function(d) {
   d$ucl_partpool <- ucl(d$theta_partpool, d$n_new)
   d

60ece9748a87ec783aaee92b06c52471a59eb939
Jens Meydam <jmeydam@gmail.com>
2020-08-04 16:16:48 +0200

    Test drive optional.

--- R/z_test_drive.R
+++ R/z_test_drive.R
@@ -1,26 +1,37 @@
-random_seed = 200731
+test_drive <- FALSE
 
-# Simulate data for use in examples
-d <- simulate_data(units = 1000, random_seed = random_seed)
+if (test_drive) {
 
-# Add no-pooling estimate of theta to data frame
-d <- add_theta_nopool(d)
+  random_seed = 200804
 
-# Add complete-pooling estimate of theta to data frame
-d <- add_theta_complpool(d)
+  # Simulate data for use in examples
+  d <- simulate_data(units = 1000, random_seed = random_seed)
 
-# Add partial-pooling estimate of theta (based on Bayesian hierarchical model)
-# to data frame
-d <- add_theta_partpool(d, random_seed = random_seed)
+  # Add no-pooling estimate of theta to data frame
+  d <- add_theta_nopool(d)
 
-# Add "true" UCL to data frame
-d <- add_ucl_true_theta(d)
+  # Add complete-pooling estimate of theta to data frame
+  d <- add_theta_complpool(d)
 
-# Add no-pooling UCL to data frame
-d <- add_ucl_nopool(d)
+  # Add partial-pooling estimate of theta (based on Bayesian hierarchical model)
+  # to data frame
+  d <- add_theta_partpool(d, random_seed = random_seed)
 
-# Add complete-pooling UCL to data frame
-d <- add_ucl_complpool(d)
+  # Add "true" UCL to data frame
+  d <- add_ucl_true_theta(d)
 
-# Add partial-pooling UCL (based on Bayesian hierarchical model) to data frame
-d <- add_ucl_partpool(d)
+  # Add no-pooling UCL to data frame
+  d <- add_ucl_nopool(d)
+
+  # Add complete-pooling UCL to data frame
+  d <- add_ucl_complpool(d)
+
+  # Add partial-pooling UCL (based on Bayesian hierarchical model) to data frame
+  d <- add_ucl_partpool(d)
+
+  print(sum(d$y_new - d$ucl_true_theta > 0))
+  print(sum(d$y_new - d$ucl_nopool > 0))
+  print(sum(d$y_new - d$ucl_complpool > 0))
+  print(sum(d$y_new - d$ucl_partpool > 0))
+
+}

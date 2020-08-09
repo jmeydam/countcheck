4e49fe23e52a61be3b1267e03179630772ab3df3
Jens Meydam <jmeydam@gmail.com>
2020-08-09 08:29:46 +0200

    Improved formatting of documentation.

--- R/data.R
+++ R/data.R
@@ -8,11 +8,13 @@
 #' countcheck(random_seed = 200807)
 #'
 #' @format A data frame with 1000 rows and 5 variables:
-#' \describe{
-#' \item{unit}{ID for unit}
-#' \item{n}{previous reference count values (measure of exposure)}
-#' \item{y}{previous count values of interest}
-#' \item{n_new}{new reference count values (measure of exposure)}
-#' \item{y_new}{new count values of interest}
+#' \itemize{
+#' \item \emph{unit}:            ID for unit
+#' \item \emph{n}:               previous reference count values (measure of
+#'                                 exposure), must at least be 1
+#' \item \emph{y}:               previous count values of interest
+#' \item \emph{n_new}:           new reference count values (measure of
+#'                                 exposure), must at least be 1
+#' \item \emph{y_new}:           new count values of interest
 #' }
 "example_01"
